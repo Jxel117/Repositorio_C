17a2f5d864d839c9ab950c335d5961a5a44e0ce1
Joel Tapia <stalin.tapia@unl.edu.ec>
2024-04-20 15:17:41 -0500

    Creación del programa HolaMundo

---
+++ HolaMundo.c
@@ -0,0 +1,7 @@
+//Programa que muestra un ensaje en la pantalla.
+#include <stdio.h>
+int main(){
+    printf("Hola Mundo\n");
+    printf("Mi nombre es: Joel Tapia, y mi alias Jxel117\n");
+    return 0;
+}
