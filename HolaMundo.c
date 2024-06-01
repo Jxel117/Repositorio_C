8dacfc0d560f1e900bb6a7852306c7ac50b05e0d
145548109 <marco.castillo@unl.edu.ec>
2024-05-31 20:21:16 -0500

    Descripcion

--- HolaMundo.c
+++ HolaMundo.c
@@ -1,7 +1,9 @@
 //Programa que muestra un ensaje en la pantalla.
+//pull
 #include <stdio.h>
 int main(){
     printf("Hola Mundo\n");
     printf("Mi nombre es: Joel Tapia, y mi alias Jxel117\n");
     return 0;
+    //brr
 }
